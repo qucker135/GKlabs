a5bfa224a849eb80172cd5e3bf41d09f747470e1
Bartosz Matysiak <qucker135@gmail.com>
2021-12-18 20:54:17 +0100

    Lab06 - 3.0 - create textured square

--- lab06/lab6.py
+++ lab06/lab6.py
@@ -93,8 +93,16 @@ def render(time):
     glVertex3f(-5.0, -5.0, 0.0)
     glTexCoord2f(1.0, 0.0)
     glVertex3f(5.0, -5.0, 0.0)
-    glTexCoord2f(0.5, 1.0)
-    glVertex3f(0.0, 5.0, 0.0)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(5.0, 5.0, 0.0)
+    
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(5.0, 5.0, 0.0)
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(-5.0, 5.0, 0.0)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(-5.0, -5.0, 0.0)
+
     glEnd()
 
     glFlush()
